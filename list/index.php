d6c4a785bc3bb41553ccc1e3abe68cd213e4e6d5
frostkom <martin.fors@frostkom.se>
2016-12-20 09:30:48 +0100

    Make plugin work without MF Group

--- list/index.php
+++ list/index.php
@@ -2,25 +2,34 @@
 
 $intGroupID = check_var('intGroupID');
 
-$obj_group = new mf_group();
+$has_group_plugin = is_plugin_active("mf_group");
+
+if($has_group_plugin)
+{
+	$obj_group = new mf_group();
+}
+
 $obj_address = new mf_address();
 $obj_address->fetch_request();
 echo $obj_address->save_data();
 
-if(isset($_GET['no_ses'])){	$is_part_of_group = check_var('is_part_of_group', 'int', true, '', false, 'get');}
-else{						$is_part_of_group = check_var('is_part_of_group', 'int');}
+if($has_group_plugin)
+{
+	if(isset($_GET['no_ses'])){	$is_part_of_group = check_var('is_part_of_group', 'int', true, '', false, 'get');}
+	else{						$is_part_of_group = check_var('is_part_of_group', 'int');}
 
-if($obj_group->id > 0){		$_SESSION['intGroupID'] = $obj_group->id;}
-else{						unset($_SESSION['intGroupID']);}
+	if($obj_group->id > 0){		$_SESSION['intGroupID'] = $obj_group->id;}
+	else{						unset($_SESSION['intGroupID']);}
 
-if($is_part_of_group){		$_SESSION['is_part_of_group'] = $is_part_of_group;}
-else{						unset($_SESSION['is_part_of_group']);}
+	if($is_part_of_group){		$_SESSION['is_part_of_group'] = $is_part_of_group;}
+	else{						unset($_SESSION['is_part_of_group']);}
+}
 
 echo "<div class='wrap'>
 	<h2>"
 		.__("Address Book", 'lang_address');
 
-		if($obj_group->id > 0)
+		if($has_group_plugin && $obj_group->id > 0)
 		{
 			echo " ".__("for", 'lang_address')." ".get_group_name($obj_group->id)." <a href='?page=mf_group/list/index.php'><i class='fa fa-lg fa-close red'></i></a>";
 		}
