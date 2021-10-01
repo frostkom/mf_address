0f8a1413e8eef69ffd6c33db4c8367658042e24c
frostkom <martin.fors@frostkom.se>
2021-10-01 11:01:23 +0200

    Display message when setting is reset

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 Plugin Name: MF Address Book
 Plugin URI: https://github.com/frostkom/mf_address
 Description: 
-Version: 3.3.11
+Version: 3.3.12
 Licence: GPLv2 or later
 Author: Martin Fors
 Author URI: https://frostkom.se
